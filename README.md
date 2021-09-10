1a98dee7e64891101133320f1173baaf9ab8110e
Ivan Djorgon <ivan.djorgon@slalom.com>
2021-09-10 17:18:07 -0400

    Added tests and updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Test framework with mocha steps and builder class pattern
 
-Scripts
-* "test": "./node_modules/mocha/bin/mocha --timeout=30000 --require mocha-steps ./build/tests/**.js",
-* "clean": "rm -rf build",
-* "build": "babel --preset es2015 -d build/ src/"
+Scripts - execute the scripts in the following order:
+1. "clean": "rm -rf build"
+2. "build": "babel --preset es2015 -d build/ src/"
+3. "test": "./node_modules/mocha/bin/mocha --timeout=30000 --require mocha-steps ./build/tests/**.js"
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -1,27 +1,44 @@
-// import puppeteer from "puppeteer";
+import { expect } from "chai";
 import { step } from "mocha-steps";
 import Page from '../builder'
 
-describe('Mocha steps demo', () => {
-    // let browser
+describe('Mocha steps tests', () => {
     let page
-    //let mobile
 
     before(async () => {
-        // browser = await puppeteer.launch({ headless: true });
         page = await Page.build('Desktop');
-        //mobile = await Page.build('Mobile');
     })
 
     after(async () => {
         await page.close();
-        //await mobile.close();
     });
 
     step('should load online banking menu page', async () => {
         await page.goto('http://zero.webappsecurity.com/index.html');
-        await page.waitAndClick('#onlineBankingMenu');
-        await page.waitFor(5000);
+        expect(await page.isElementVisible('#signin_button')).to.be.true;
+    });
+
+    step('should display login form', async () => {
+        await page.waitAndClick('#signin_button');
+        expect(await page.isElementVisible('#login_form')).to.be.true;
+        expect(await page.isElementVisible('#signin_button')).to.be.false;
+    });
+
+    step('should not login to application', async () => {
+        await page.waitAndType('#user_login', 'username');
+        await page.waitAndType('#user_password', 'password');
+        await page.waitAndClick('.btn-primary');
+        expect(await page.isElementVisible('.nav-tabs')).to.be.false;
+    });
+
+    step('should have text More Services under Online Banking section', async () => {
+        await page.goto('http://zero.webappsecurity.com/index.html');
+        expect(await page.getText('#online-banking')).to.equal('More Services');
+    });
+
+    step('should have 3 nav bar links', async () => {
+        await page.goto('http://zero.webappsecurity.com/index.html');
+        expect(await page.getCount('#pages-nav > li')).to.equal(3);
     });
 
 });
